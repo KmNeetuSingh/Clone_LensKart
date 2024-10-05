896b69c6569b0e23157b893934a4f6b4d0a66feb
Neetu Singh <neetuisngh99590@gmail.com>
2024-10-05 08:43:58 +0530

    js data completed data pushed

---
+++ lenskart/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, magnam?
+        </p> 
+        
+           </>
+  )
+}
+
+export default App
